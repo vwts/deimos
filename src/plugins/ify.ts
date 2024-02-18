4c50b63997b8acd934e632709db978bda9890ae9
grivo <gyket.pentest.133971@protonmail.com>
2024-02-18 15:59:31 -0300

    adição do plugin `ify`

---
+++ src/plugins/ify.ts
@@ -0,0 +1,29 @@
+import definePlugin from '../utils/types';
+
+export default definePlugin({
+    name: "ify",
+    description: "desabilita auto-pausa e checks de premium do spotify",
+    author: "vuwints",
+
+    patches: [
+        {
+            find: '.displayName="SpotifyStore"',
+
+            replacement: {
+                match: /\.isPremium=.;/,
+
+                replace: ".isPremium=true;"
+            }
+        },
+
+        {
+            find: '.displayName="SpotifyStore"',
+
+            replacement: ["SPEAKING", "VOICE_STATE_UPDATES", "MEDIA_ENGINE_SET_DESKTOP_SOURCE"].map(event => ({
+                match: new RegExp(`${event}:function\\(.\\){.+?}(,|}\\))`),
+
+                replace: (_, ending) => `${event}:function(){}${ending}`
+            }))
+        }
+    ]
+});
