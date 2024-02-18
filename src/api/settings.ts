1b522c4e6bb3e80336140bbe07adaedc4e68c9c3
grivo <gyket.pentest.133971@protonmail.com>
2024-02-18 17:04:48 -0300

    api de ouvinte de configuração aprimorado

--- src/api/settings.ts
+++ src/api/settings.ts
@@ -49,7 +49,7 @@ try {
     var settings = mergeDefaults({} as Settings, DefaultSettings);
 }
 
-type SubscriptionCallback = ((newValue: any) => void) & {
+type SubscriptionCallback = ((newValue: any, path: string) => void) & {
     _path?: string;
 };
 
@@ -75,7 +75,7 @@ function makeProxy(settings: Settings, root = settings, path = ""): Settings {
 
             for (const subscription of subscriptions) {
                 if (!subscription._path || subscription._path === setPath) {
-                    subscription(v);
+                    subscription(v, setPath);
                 }
             }
 
@@ -113,11 +113,20 @@ type ResolvePropDeep<T, P> = P extends "" ? T :
     P extends `${infer Pre}.${infer Suf}`
     ? Pre extends keyof T ? ResolvePropDeep<T[Pre], Suf> : never : P extends keyof T ? T[P] : never;
 
-export function addSettingsListener<Path extends keyof Settings>(path: Path, onUpdate: (newValue: Settings[Path]) => void): void;
-export function addSettingsListener<Path extends string>(path: Path, onUpdate: (newValue: ResolvePropDeep<Settings, Path>) => void): void;
+/**
+ * adiciona um ouvinte de configurações que será invocado sempre que a configuração desejada for atualizada
+ * 
+ * @param path caminho para a configuração que você deseja assistir
+ * @param onUpdate função callback sempre que um caminho que coincida for atualizado
+ * 
+ * @example addSettingsListener("", (newValue, path) => console.log(`${path} is now ${newValue}`))
+ *          addSettingsListener("plugins.Unindent.enabled", v => console.log("unindent é agora", v ? "enabled" : "disabled"))
+ */
+export function addSettingsListener<Path extends keyof Settings>(path: Path, onUpdate: (newValue: Settings[Path], path: Path) => void): void;
+export function addSettingsListener<Path extends string>(path: Path, onUpdate: (newValue: Path extends "" ? any : ResolvePropDeep<Settings, Path>, path: Path extends "" ? string : Path) => void): void;
 
-export function addSettingsListener(path: string, onUpdate: (newValue: any) => void) {
+export function addSettingsListener(path: string, onUpdate: (newValue: any, path: string) => void) {
     (onUpdate as SubscriptionCallback)._path = path;
-    
+
     subscriptions.add(onUpdate);
 }
