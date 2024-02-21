224f36d66f48d361b42b6f54986f750a18866476
grivo <gyket.pentest.133971@protonmail.com>
2024-02-21 20:32:42 -0300

    correção do plugin `viewprofileimages`

--- src/plugins/viewIcons.tsx
+++ src/plugins/viewIcons.tsx
@@ -1,44 +1,29 @@
-import {
-    Modal,
-    openModal
-} from '../utils/modal';
-
-import {
-    filters,
-    waitFor
-} from '../webpack';
-
+import IpcEvents from '../utils/IpcEvents';
 import definePlugin from '../utils/types';
-
-let ImageModal: any;
-let renderMaskedLink: any;
-
-waitFor(filters.byDisplayName("ImageModal"), m => ImageModal = m.default);
-waitFor("renderMaskedLinkComponent", m => renderMaskedLink = m.renderMaskedLinkComponent);
 
 const OPEN_URL = "Deimos.Plugins.plugins.ViewIcons.openImage(";
 
 export default definePlugin({
     name: "viewicons",
-    description: "faz dos avatares/banners dos usuários, clicáveis, e adiciona entradas de menu de contexto para servidores para ver banner/ícone. (crasha se você não possuir o modo desenvolvedor ativado, correção em breve).",
+    description: "faz dos avatares/banners dos usuários, clicáveis, e adiciona entradas de menu de contexto para servidores para ver banner/ícone.",
     author: "vuwints",
 
     openImage(url: string) {
-        openModal(() => (
+        DeimosNative.ipc.invoke(IpcEvents.OPEN_EXTERNAL, url);
+
+		/* openModal(() => (
             <ImageModal
                 shouldAnimate={true}
                 original={url}
                 src={url}
                 renderLinkComponent={renderMaskedLink}
             />
-        ), {
-            size: Modal.ModalSize.DYNAMIC
-        });
+        ), { size: Modal.ModalSize.DYNAMIC }); */
     },
 
     patches: [
         {
-            find: "UserProfileModalHeader",
+            find: "onAddFriend:",
 
             replacement: {
                 match: /\{src:(.{1,2}),avatarDecoration/,
@@ -48,17 +33,17 @@ export default definePlugin({
         },
 
         {
-            find: "default.popoutNoBannerPremium",
+            find: "().popoutNoBannerPremium",
 
             replacement: {
-                match: /style:.{1,2}\(\{\},(.{1,2}),/,
+                match: /style:.{0,10}\{\},(.{1,2})\)/,
 
                 replace: (m, bannerObj) => `onClick:${bannerObj}.backgroundImage&&(()=>${OPEN_URL}${bannerObj}.backgroundImage.replace("url(", "").replace(/(\\?size=.+)?\\)/, "?size=2048"))),${m}`
             }
         },
 
         {
-            find: "GuildContextMenuWrapper",
+            find: "GuildContextMenu:",
 
             replacement: [
                 {
@@ -68,9 +53,9 @@ export default definePlugin({
                 },
 
                 {
-                    match: /,(.{1,2})\((.{1,2})\.MenuGroup,\{\},void 0,(.{1,2})\)(?=\)\}.{1,2}\.displayName)/,
+                    match: /(?<=createElement\((.{1,5}),\{id:"leave-guild".{0,100}\,)(.{1,2}\.createElement)\((.{1,5}),null,(.{1,2})\)(?=\)\}function)/,
 
-                    replace: (_, createElement, menu, copyIdElement) => `,${createElement}(${menu}.MenuGroup,{},void 0,[` + `_guild.icon&&${createElement}(${menu}.MenuItem,` + `{id:"viewicons-copy-icon",label:"ver ícone",action:()=>${OPEN_URL}_guild.getIconURL(void 0,true)+"size=2048")}),` + `_guild.banner&&${createElement}(${menu}.MenuItem,` + `{id:"viewicons-copy-banner",label:"ver banner",action:()=>${OPEN_URL}Deimos.Webpack.findByProps("getGuildBannerURL").getGuildBannerURL(_guild).replace(/\\?size=.+/, "?size=2048"))}),${copyIdElement}])`
+                    replace: (_, menu, createElement, menuGroup, copyIdElement) => `${createElement}(${menuGroup},null,[` + `_guild.icon&&${createElement}(${menu},` + `{id:"viewicons-copy-icon",label:"ver ícone",action:()=>${OPEN_URL}_guild.getIconURL(void 0,true)+"size=2048")}),` + `_guild.banner&&${createElement}(${menu},` + `{id:"viewicons-copy-banner",label:"ver banner",action:()=>${OPEN_URL}Deimos.Webpack.findByProps("getGuildBannerURL").getGuildBannerURL(_guild).replace(/\\?size=.+/, "?size=2048"))})` + `,${copyIdElement}])`
                 }
             ]
         }
