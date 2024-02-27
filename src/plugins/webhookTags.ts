cfbbff6f280e7ca5906bf2ea36bc034f126aa414
grivo <gyket.pentest.133971@protonmail.com>
2024-02-27 18:03:05 -0300

    adição do plugin `webhooktags`

---
+++ src/plugins/webhookTags.ts
@@ -0,0 +1,42 @@
+import {
+	Devs
+} from '../utils/constants';
+
+import definePlugin from '../utils/types';
+
+export default definePlugin({
+	name: "webhook tags",
+	description: "",
+
+	authors: [Devs.Vuw],
+
+	patches: [
+		{
+			find: '.BOT=0]="BOT"',
+
+			replacement: [
+				{
+					match: /(.)\[.\.BOT=0\]="BOT";/,
+
+					replace: (orig, types) => `${types}[${types}.WEBHOOK=99]="WEBHOOK";${orig}`
+				},
+
+				{
+					match: /case (.)\.BOT:default:(.)=/,
+
+                    replace: (orig, types, text) => `case ${types}.WEBHOOK:${text}="WEBHOOK";break;${orig}`
+				}
+			]
+		},
+
+		{
+			find: ".Types.ORIGINAL_POSTER",
+
+            replacement: {
+                match: /return null==(.)\?null:.\.createElement\((.)\.Z/,
+
+                replace: (orig, type, BotTag) => `if(${type}==${BotTag}.Z.Types.BOT&&arguments[0].user.isNonUserBot()){${type}=${BotTag}.Z.Types.WEBHOOK}${orig}`
+            }
+		}
+	]
+});
