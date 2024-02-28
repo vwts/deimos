76d07d3139e6b62c607f443ad30aa7eca329c9ad
grivo <gyket.pentest.133971@protonmail.com>
2024-02-28 14:27:12 -0300

    plugin que remove os sub-domínios canary

---
+++ src/plugins/noCanaryMessageLinks.ts
@@ -0,0 +1,33 @@
+import {
+	addPreSendListener,
+	MessageObject,
+	removePreSendListener
+} from '../api/MessageEvents';
+
+import definePlugin from '../utils/types';
+
+export default definePlugin({
+	name: "nocanarymessagelinks",
+	description: "",
+
+	authors: [
+		{
+			name: "vuwints",
+			id: 671809749955641364n
+		}
+	],
+
+	dependencies: ["MessageEventsAPI"],
+
+	removeBetas(msg: MessageObject) {
+		msg.content = msg.content.replace(/(?<=https:\/\/)(canary.|ptb.)(?=discord(?:app)?.com\/channels\/(?:\d{17,20}|@me)\/\d{17,20}\/\d{17,20})/g, ""); // dei w
+	},
+
+	start() {
+		this.preSend = addPreSendListener((_, msg) => this.removeBetas(msg));
+	},
+
+	stop() {
+		removePreSendListener(this.preSend);
+	}
+});
